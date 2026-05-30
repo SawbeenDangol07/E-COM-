e29df39fc53e453c027edd4fe2ef503af9d07a0e
SawbeenDangol07 <sabindangol1010@gmail.com>
2026-05-30 09:38:53 +0545

    category added

--- src/modules/categories/categories.controller.js
+++ src/modules/categories/categories.controller.js
@@ -1,14 +1,74 @@
+const categoryService = require("./categories.service");
 const { UserRoles } = require("../../config/constant");
-const categoriesService = require("./categories.service");
 
-class categoryController {
+class CategoryController {
   async create(req, res, next) {
     try {
-      const data = await categoriesService.transformToCategoryData(req);
-      const category = await categoriesService.storeCategory(data);
+      const data = await categoryService.transformToCategoryCreate(req);
+      const category = await categoryService.storeCategory(data);
+
       res.json({
         data: category,
-        message: "Category added sucessfully",
+        message: "Category Created Successfully",
+        status: "CATEGORY_CREATED",
+      });
+    } catch (exception) {
+      next(exception);
+    }
+  }
+
+  async listAll(req, res, next) {
+    try {
+      let filter = {};
+      // let searchKeyword = req.query.search || req.query.keyword;
+      if (req.query.search) {
+        filter = {
+          $or: [{ name: new RegExp(req.query.search, "i") }],
+        };
+      }
+      if (req.query.status) {
+        filter = {
+          ...filter,
+          status: req.query.status,
+        };
+      }
+
+      const config = {
+        page: +req.query.page || 1,
+        limit: +req.query.limit || 20,
+      };
+
+      const { data, pagination } = await categoryService.getAllRowsByFilter(
+        filter,
+        config,
+      );
+      res.json({
+        data: data,
+        message: "Category list fetched successfully",
+        status: "CATEGORY_LIST_FETCHED",
+        meta: { pagination },
+      });
+    } catch (exception) {
+      next(exception);
+    }
+  }
+
+  async getDetail(req, res, next) {
+    try {
+      const category = await categoryService.getSingleRowsByFilter({
+        _id: req.params.categoryId,
+      });
+      if (!category) {
+        throw {
+          code: 404,
+          message: "Category not found",
+          status: "CATEGORY_NOT_FOUND",
+        };
+      }
+
+      res.json({
+        data: category,
+        message: "Category fetched successfully",
         status: "OK",
       });
     } catch (exception) {
@@ -16,20 +76,97 @@ class categoryController {
     }
   }
 
-  async getAllCategories() {
+  async update(req, res, next) {
     try {
-      let filter = {};
-      if (req.loggedInUser.role !== UserRoles.ADMIN) {
+      const category = await categoryService.getSingleRowsByFilter({
+        _id: req.params.categoryId,
+      });
+      if (!category) {
+        throw {
+          code: 404,
+          message: "Category not found",
+          status: "CATEGORY_NOT_FOUND",
+        };
+      }
+
+      const data = await categoryService.transformToCategoryUpdate(
+        req,
+        category,
+      );
+
+      const update = await categoryService.updateSingleRowByFilter(
+        { _id: category._id },
+        data,
+      );
+
+      res.json({
+        data: update,
+        message: "Category Update Successful",
+        status: "OK",
+      });
+    } catch (exception) {
+      next(exception);
+    }
+  }
+
+  async delete(req, res, next) {
+    try {
+      const loggedInUser = req.loggedInUser;
+
+      let filter = {
+        _id: req.params.categoryId,
+      };
+
+      if (loggedInUser.role !== UserRoles.ADMIN) {
         filter = {
           ...filter,
-          createdBy: req.loggedInUser._id,
-        };``
+          createdBy: loggedInUser._id,
+        };
       }
+      const category = await categoryService.getSingleRowsByFilter(filter);
+
+      if (!category) {
+        throw {
+          code: 404,
+          message: "Category not Found",
+          status: "CATEGORY_NOT_FOUND_ERR",
+        };
+      }
+
+      const del = await categoryService.delete(filter);
+
+      res.json({
+        data: del,
+        message: "Category Delted Successfully",
+        status: "OK",
+      });
+    } catch (exception) {
+      next(exception);
+    }
+  }
+
+  async getDetailBySlug(req, res, next) {
+    try {
+      const categoryDetail = await categoryService.getSingleRowsByFilter({
+        slug: req.params.slug,
+      });
+      //to list all the products associate with this category
+
+      res.json({
+        data: {
+          category: categoryDetail,
+          products: [],
+        },
+        message: "Category detail",
+        status: "OK",
+        meta: {},
+      });
     } catch (exception) {
       next(exception);
     }
   }
 }
 
-const categoryCtrl = new categoryController();
+const categoryCtrl = new CategoryController();
+
 module.exports = categoryCtrl;
--- src/modules/categories/categories.service.js
+++ src/modules/categories/categories.service.js
@@ -1,29 +1,47 @@
-const slugify = require("slugify");
-const cloudinaryService = require("../../Services/cloudinary.service");
-const categoryModel = require("./categories.model");
+const CloudinaryService = require("./../../Services/cloudinary.service");
 
-class catergoryService {
-  async transformToCategoryData(req) {
+const CategoryModel = require("./categories.model");
+const slugify = require("slugify");
+class CategoryService {
+  async transformToCategoryCreate(req) {
     try {
       const data = req.body;
       data.slug = slugify(data.name, {
         lower: true,
+        trim: true,
+        strict: true,
+        remove: true,
       });
-
       if (req.file) {
-        data.image = await cloudinaryService.singleFileUpload(
+        data.logo = await CloudinaryService.singleFileUpload(
           req.file.path,
-          "category",
+          "/category",
         );
       }
 
-      if (!data.parent || data.parent === "" || data.parent === "null") {
-        data.parent = null;
-      }
-      if (!data.brands || data.brands === "" || data.brands === "null") {
-        data.brands = null;
-      }
       data.createdBy = req.loggedInUser._id;
+
+      return data;
+    } catch (exception) {
+      throw exception;
+    }
+  }
+
+  async transformToCategoryUpdate(req, category) {
+    try {
+      const data = req.body;
+
+      if (req.file) {
+        data.logo = await CloudinaryService.singleFileUpload(
+          req.file.path,
+          "/category",
+        );
+      } else {
+        data.logo = category.logo;
+      }
+
+      data.updatedBy = req.loggedInUser._id;
+
       return data;
     } catch (exception) {
       throw exception;
@@ -32,12 +50,95 @@ class catergoryService {
 
   async storeCategory(data) {
     try {
-      let category = new categoryModel(data);
+      const category = new CategoryModel(data);
       return await category.save();
+    } catch (exception) {
+      throw exception;
+    }
+  }
+
+  async getAllRowsByFilter(filter, config = { page: 1, limit: 20 }) {
+    try {
+      const page = config.page || 1;
+      const limit = +config.limit || 20;
+
+      const skip = (page - 1) * limit;
+
+      const total = await CategoryModel.countDocuments(filter);
+
+      const data = await CategoryModel.find(filter)
+        .populate("createdBy", [
+          "_id",
+          "name",
+          "email",
+          "role",
+          "image",
+          "status",
+        ])
+        .populate("updatedBy", [
+          "_id",
+          "name",
+          "email",
+          "role",
+          "image",
+          "status",
+        ])
+        .skip(skip)
+        .limit(limit);
+
+      return { data, pagination: { page: page, limit: limit, total: total } };
+    } catch (exception) {
+      throw exception;
+    }
+  }
+
+  async getSingleRowsByFilter(filter) {
+    try {
+      const data = await CategoryModel.findOne(filter)
+        .populate("createdBy", [
+          "_id",
+          "name",
+          "email",
+          "role",
+          "image",
+          "status",
+        ])
+        .populate("updatedBy", [
+          "_id",
+          "name",
+          "email",
+          "role",
+          "image",
+          "status",
+        ]);
+
+      return data;
+    } catch (exception) {
+      throw exception;
+    }
+  }
+
+  async updateSingleRowByFilter(filter, data) {
+    try {
+      const updateResponse = await CategoryModel.findOneAndUpdate(
+        filter,
+        { $set: data },
+        { new: true },
+      );
+      return updateResponse;
+    } catch (exception) {
+      throw exception;
+    }
+  }
+
+  async delete(filter) {
+    try {
+      const del = await CategoryModel.findOneAndDelete(filter);
+      return del;
     } catch (exception) {
       throw exception;
     }
   }
 }
 
-module.exports = new catergoryService();
+module.exports = new CategoryService();
